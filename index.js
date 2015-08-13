8f40206bc3e9a75f32cd5cc80ea3217281d12b09
Vladimir Starkov <iamstarkov@gmail.com>
2015-08-13 20:39:57 +0200

    add implementation

--- index.js
+++ index.js
@@ -1,3 +1,41 @@
+import { is } from 'ramda';
+
+const isOwn = tweet => !isReply(tweet) && !isRetweet(tweet);
+const isRetweet = tweet => !!tweet.retweeted_status;
+const isReplyToSelf = tweet => tweet.in_reply_to_screen_name === 'jsunderhood';
+const isReply = tweet => !!tweet.in_reply_to_screen_name && !isReplyToSelf(tweet);
+const sumRetweeted = (state, tweet) => state + tweet.retweet_count;
+const sumFavorited = (state, tweet) => state + tweet.favorite_count;
+
 export default function tweetsStats(input) {
-  return input;
+  if (!input || !is(Array, input)) return;
+
+  var rt = 0;
+  var fav = 0;
+
+  const _tweets = input;
+
+  const tweets = _tweets.length;
+  const _ownTweets = _tweets.filter(isOwn);
+
+  const percent = tweets / 100;
+  const own = _ownTweets.length;
+  const replies = _tweets.filter(isReply).length;
+  const retweets = _tweets.filter(isRetweet).length;
+
+  const ownPercentage = (own / percent) || 0;
+  const repliesPercentage = (replies / percent) || 0;
+  const retweetsPercentage = (retweets / percent) || 0;
+
+  const retweeted = _ownTweets.reduce(sumRetweeted, 0);
+  const favorited = _ownTweets.reduce(sumFavorited, 0);
+  const retweetedKpi = Number(((retweeted / own) || 0).toFixed(2));
+  const favoritedKpi = Number(((favorited / own) || 0).toFixed(2));
+
+  return {
+    tweets, own, replies, retweets,
+    ownPercentage, repliesPercentage, retweetsPercentage,
+    retweeted, favorited,
+    retweetedKpi, favoritedKpi
+  };
 };
--- test.js
+++ test.js
@@ -1,8 +1,53 @@
 import { equal } from 'assert';
-import tweetsStats from './index';
+import stats from './index';
+import { own, replies, retweets } from './fixtures.json';
 
-it('should tweetsStats', () =>
-  equal(tweetsStats('unicorns'), 'unicorns'));
+const tweets = [...own, ...replies, ...retweets];
+const percent = tweets.length / 100;
 
-it('should tweetsStats invalid input', () =>
-  equal(tweetsStats(), undefined));
+it('should stats tweets', () =>
+  equal(stats(tweets).tweets, tweets.length));
+
+it('should stats own', () =>
+  equal(stats(tweets).own, own.length));
+
+it('should stats replies', () =>
+  equal(stats(tweets).replies, replies.length));
+
+it('should stats retweets', () =>
+  equal(stats(tweets).retweets, retweets.length));
+
+it('should stats ownPercentage', () =>
+  equal(stats(tweets).ownPercentage, replies.length / percent));
+
+it('should stats repliesPercentage', () =>
+  equal(stats(tweets).repliesPercentage, replies.length / percent));
+
+it('should stats retweetsPercentage', () =>
+  equal(stats(tweets).retweetsPercentage, retweets.length / percent));
+
+it('should stats retweeted', () =>
+  equal(stats(tweets).retweeted, 121));
+
+it('should stats retweetedKpi', () =>
+  equal(stats(tweets).retweetedKpi, 60.5));
+
+it('should stats favorited', () =>
+  equal(stats(tweets).favorited, 5));
+
+it('should stats favoritedKpi', () =>
+  equal(stats(tweets).favoritedKpi, 2.5));
+
+it('should stats invalid input', () =>
+  equal(stats('unicorns'), undefined));
+
+it('should stats invalid empty input', () =>
+  equal(stats(), undefined));
+
+it('should stats epmty array outcome valid props', () => {
+  equal(stats([]).ownPercentage, 0);
+  equal(stats([]).repliesPercentage, 0);
+  equal(stats([]).retweetsPercentage, 0);
+  equal(stats([]).retweetedKpi, 0);
+  equal(stats([]).favoritedKpi, 0);
+});
